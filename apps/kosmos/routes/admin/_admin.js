3b1232ba6b6ebd067459c9d1ab543b6ca23443a4
ponyspy <desade4me@gmail.com>
2017-04-20 23:59:10 +0300

    views

--- apps/kosmos/routes/admin/_admin.js
+++ apps/kosmos/routes/admin/_admin.js
@@ -6,6 +6,10 @@ var upload = multer({ dest: __glob_root + '/uploads/' });
 var admin = {
 	main: require('./main.js'),
 	works: require('./works/_works.js'),
+	publications: require('./publications/_publications.js'),
+	awards: require('./awards/_awards.js'),
+	events: require('./events/_events.js'),
+	peoples: require('./peoples/_peoples.js'),
 	cv: require('./cv.js'),
 	users: require('./users/_users.js'),
 	options: require('./options.js')
@@ -27,6 +31,10 @@ module.exports = (function() {
 		.post(checkAuth, admin.cv.edit_form);
 
 	router.use('/works', checkAuth, upload.single('poster'), admin.works);
+	router.use('/publications', checkAuth, upload.single('poster'), admin.publications);
+	router.use('/awards', checkAuth, admin.awards);
+	router.use('/events', checkAuth, admin.events);
+	router.use('/peoples', checkAuth, admin.peoples);
 	router.use('/users', checkAuth, admin.users);
 
 	router.post('/preview', checkAuth, upload.single('image'), admin.options.preview);
