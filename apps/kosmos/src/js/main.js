67c5902582617d2400821e4d31a701ed2e5e30ab
ponyspy <desade4me@gmail.com>
2017-04-30 22:58:33 +0300

    timer

--- apps/kosmos/src/js/main.js
+++ apps/kosmos/src/js/main.js
@@ -1,3 +1,20 @@
 $(function() {
 
+	var timer = function() {
+		$('.active').trigger('click');
+	};
+
+	var interval = setInterval(timer, 3000);
+
+	var $images = $('.poster_item').on('click', function(e) {
+
+		clearInterval(interval);
+		interval = setInterval(timer, 3000);
+
+		$(this).index() < $images.length - 1
+			? $images.removeClass('active').filter(this).next().addClass('active')
+			: $images.removeClass('active').first().addClass('active');
+
+	}).first().addClass('active').end();
+
 });
