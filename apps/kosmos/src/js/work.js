37cddbecadeef4c13e65c2b46f3dafae37323bb9
ponyspy <desade4me@gmail.com>
2017-05-02 16:07:42 +0300

    init

--- apps/kosmos/src/js/work.js
+++ apps/kosmos/src/js/work.js
@@ -2,26 +2,13 @@ $(function() {
 
 	$('.gallery-block').each(function() {
 		var $gallery_top = $(this).children('.gallery-top');
-		var $gallery_thumbs = $(this).children('.gallery-thumbs');
 
 		var galleryTop = new Swiper($gallery_top, {
-			initialSlide: 2000,
-			// nextButton: '.swiper-button-next',
-			// prevButton: '.swiper-button-prev',
-			spaceBetween: 10,
+			initialSlide: 0,
+			nextButton: '.swiper-button-next',
+			prevButton: '.swiper-button-prev',
+			spaceBetween: 40,
 		});
-
-		var galleryThumbs = new Swiper($gallery_thumbs, {
-			spaceBetween: 10,
-			centeredSlides: true,
-			initialSlide: 2000,
-			slidesPerView: 'auto',
-			touchRatio: 0.2,
-			slideToClickedSlide: true
-		});
-
-		galleryTop.params.control = galleryThumbs;
-		galleryThumbs.params.control = galleryTop;
 
 	});
 
