181b87bc0a4416f1002a7e863592e074dff79f26
ponyspy <desade4me@gmail.com>
2017-05-02 16:55:32 +0300

    size

--- apps/kosmos/src/js/work.js
+++ apps/kosmos/src/js/work.js
@@ -4,10 +4,14 @@ $(function() {
 		var $gallery_top = $(this).children('.gallery-top');
 
 		var galleryTop = new Swiper($gallery_top, {
-			initialSlide: 0,
+			// initialSlide: 0,
+			keyboardControl: true,
 			nextButton: '.swiper-button-next',
 			prevButton: '.swiper-button-prev',
 			spaceBetween: 40,
+			autoHeight: true,
+			slidesPerView: 1,
+			// centeredSlides: true,
 		});
 
 	});
