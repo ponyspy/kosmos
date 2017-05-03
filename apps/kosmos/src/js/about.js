2176b3d47b70281db78bd8ececb0510020d07119
ponyspy <desade4me@gmail.com>
2017-05-03 23:46:13 +0300

    about

--- apps/kosmos/src/js/about.js
+++ apps/kosmos/src/js/about.js
@@ -1,3 +1,13 @@
 $(function() {
+		var $gallery_press = $('.gallery-press');
 
+		var galleryPress = new Swiper($gallery_press, {
+			keyboardControl: true,
+			pagination: '.swiper-pagination',
+			paginationClickable: true,
+			spaceBetween: 40,
+			slidesPerView: 3,
+			// autoHeight: true,
+			// centeredSlides: true,
+		});
 });
