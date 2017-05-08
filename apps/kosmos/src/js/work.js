527345346a4ac3dbfee7eef8da25ea862e87b351
ponyspy <desade4me@gmail.com>
2017-05-08 12:50:39 +0300

    fix

--- apps/kosmos/src/js/work.js
+++ apps/kosmos/src/js/work.js
@@ -4,14 +4,16 @@ $(function() {
 		var $gallery = $(this).children('.swiper-container');
 
 		var gallery = new Swiper($gallery, {
-			// initialSlide: 0,
 			keyboardControl: true,
 			nextButton: '.swiper-button-next',
 			prevButton: '.swiper-button-prev',
 			spaceBetween: 40,
 			autoHeight: true,
 			slidesPerView: 1,
-			// centeredSlides: true,
+			// preloadImages: false,
+			// lazyLoading: true,
+			// lazyLoadingInPrevNext: true,
+			// lazyLoadingInPrevNextAmount: 2
 		});
 
 	});
@@ -19,13 +21,7 @@ $(function() {
 	var bLazy = new Blazy({
 		offset: 100,
 		selector: '.image',
-		successClass: 'load',
-		breakpoints: [
-			{
-				width: 420,
-				src: 'data-src-small'
-			}
-		]
+		successClass: 'loaded'
 	});
 
 });
