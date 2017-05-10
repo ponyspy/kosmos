4b9e7599b50958c13b9df5b6abffed7ffcacbd14
ponyspy <desade4me@gmail.com>
2017-05-10 12:29:05 +0300

    lazy test

--- apps/kosmos/src/js/main.js
+++ apps/kosmos/src/js/main.js
@@ -6,6 +6,8 @@ $(function() {
 
 	var interval = setInterval(timer, 3000);
 
+	// Not Lazy load
+
 	var $images = $('.poster_item').on('click', function(e) {
 
 		clearInterval(interval);
@@ -16,5 +18,23 @@ $(function() {
 			: $images.removeClass('active').first().addClass('active');
 
 	}).first().addClass('active').end();
+
+	// Lazy load
+
+	// var $images = $('.poster_item');
+
+	// $images.on('click', function(e) {
+	// 	var $this = $(this);
+
+	// 	clearInterval(interval);
+	// 	interval = setInterval(timer, 3000);
+
+	// 	$this.index() < $images.length - 1
+	// 		? $images.removeClass('active').filter(this).next().not('.load').attr('src', $this.next().attr('data-src')).end().addClass('active load')
+	// 		: $images.removeClass('active').first().not('.load').attr('src', $images.first().attr('data-src')).end().addClass('active load');
+
+
+	// }).last().trigger('click');
+
 
 });
