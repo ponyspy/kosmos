c111b0c1de316f0921b913de23d0d1409847eb63
ponyspy <desade4me@gmail.com>
2017-05-14 13:24:13 +0300

    lazy

--- apps/kosmos/src/js/main.js
+++ apps/kosmos/src/js/main.js
@@ -8,33 +8,33 @@ $(function() {
 
 	// Not Lazy load
 
-	// var $images = $('.poster_item').on('click', function(e) {
-
-	// 	clearInterval(interval);
-	// 	interval = setInterval(timer, 3000);
-
-	// 	$(this).index() < $images.length - 1
-	// 		? $images.removeClass('active').filter(this).next().addClass('active')
-	// 		: $images.removeClass('active').first().addClass('active');
-
-	// }).first().addClass('active').end();
-
-	// Lazy load
-
-	var $images = $('.poster_item');
-
-	$images.on('click', function(e) {
-		var $this = $(this);
+	var $images = $('.poster_item').on('click', function(e) {
 
 		clearInterval(interval);
 		interval = setInterval(timer, 3000);
 
-		$this.index() < $images.length - 1
-			? $images.removeClass('active').filter(this).next().not('.load').attr('src', $this.next().attr('data-src')).end().addClass('active load')
-			: $images.removeClass('active').first().not('.load').attr('src', $images.first().attr('data-src')).end().addClass('active load');
+		$(this).index() < $images.length - 1
+			? $images.removeClass('active').filter(this).next().addClass('active')
+			: $images.removeClass('active').first().addClass('active');
+
+	}).first().addClass('active').end();
+
+	// Lazy load
+
+	// var $images = $('.poster_item');
+
+	// $images.on('click', function(e) {
+	// 	var $this = $(this);
+
+	// 	clearInterval(interval);
+	// 	interval = setInterval(timer, 3000);
+
+	// 	$this.index() < $images.length - 1
+	// 		? $images.removeClass('active').filter(this).next().not('.load').attr('src', $this.next().attr('data-src')).end().addClass('active load')
+	// 		: $images.removeClass('active').first().not('.load').attr('src', $images.first().attr('data-src')).end().addClass('active load');
 
 
-	}).last().trigger('click');
+	// }).last().trigger('click');
 
 
 });
