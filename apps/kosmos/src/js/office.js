85796b71433ce037cd5b907177feebf7c400de16
ponyspy <desade4me@gmail.com>
2017-08-01 20:42:55 +0300

    press

--- apps/kosmos/src/js/office.js
+++ apps/kosmos/src/js/office.js
@@ -1,23 +1,5 @@
 $(function() {
-		var $gallery_press = $('.gallery-press');
-
-		var galleryPress = new Swiper($gallery_press, {
-			keyboardControl: true,
-			pagination: '.swiper-pagination',
-			paginationClickable: true,
-			spaceBetween: 80,
-			slidesPerView: 3,
-			// autoHeight: true,
-			// centeredSlides: true,
-			breakpoints: {
-				680: {
-					slidesPerView: 1,
-					spaceBetween: 10
-				},
-			}
-		});
-
-		$('.block_cut').on('click', function() {
-			$(this).nextAll().removeClass('hidden').end().remove();
-		});
+	$('.block_cut').on('click', function() {
+		$(this).parent().find('.hidden').removeClass('hidden').end().end().remove();
+	});
 });
