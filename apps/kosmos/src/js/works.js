8c6cdc3af92946147abde8f46fd52dd2d362119c
ponyspy <desade4me@gmail.com>
2017-08-03 11:01:24 +0300

    fix

--- apps/kosmos/src/js/works.js
+++ apps/kosmos/src/js/works.js
@@ -1,6 +1,6 @@
 $(function() {
 	var $window = $(window);
-	var type = $('body').attr('class').split(' ')[0] == 'project' ? 'projects' : 'research';
+	var work_type = $('body').attr('class').split(' ')[0] == 'project' ? 'projects' : 'research';
 	var context = {
 		skip: 0,
 		limit: 4,
@@ -10,7 +10,7 @@ $(function() {
 	function scrollLoader(e) {
 		if ($window.scrollTop() + $window.height() + 200 >= $(document).height()) {
 			$window.off('scroll');
-			$.ajax({url: '/' + type, method: 'POST', data: { context: context }, async: false }).done(function(data) {
+			$.ajax({url: '/' + work_type, method: 'POST', data: { context: context }, async: false }).done(function(data) {
 				if (data !== 'end') {
 					$('.works_block').append(data).promise().done(function() {
 						$('.category_item').removeClass('current').filter('.' + context.category).addClass('current');
@@ -29,7 +29,7 @@ $(function() {
 			context.limit = 4;
 			context.category = window.location.hash === '' ? 'all' : window.location.hash.replace('#', '');
 
-			$.ajax({url: '/' + type, method: 'POST', data: { context: context }, async: false }).done(function(data) {
+			$.ajax({url: '/' + work_type, method: 'POST', data: { context: context }, async: false }).done(function(data) {
 				if (data !== 'end') {
 					var elems = $(data);
 					context.skip = elems.length;
