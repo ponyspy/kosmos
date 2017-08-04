b443a267264e1eadac50999a69088ccc17ce947f
ponyspy <desade4me@gmail.com>
2017-08-04 10:28:55 +0300

    types

--- apps/kosmos/routes/admin/peoples/list.js
+++ apps/kosmos/routes/admin/peoples/list.js
@@ -26,6 +26,10 @@ module.exports = function(Model) {
 			? People.find({ $text : { $search : post.context.text } } )
 			: People.find();
 
+		if (post.context.type && post.context.type != 'all') {
+			Query.where('type').equals(post.context.type);
+		}
+
 		if (post.context.status && post.context.status == 'default') {
 			Query.where('status').ne('hidden');
 		}
