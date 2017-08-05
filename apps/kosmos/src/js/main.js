4ae4b91f207abf263c5fae60f214565ed06fe0d7
ponyspy <desade4me@gmail.com>
2017-08-05 17:24:32 +0300

    init

--- apps/kosmos/src/js/main.js
+++ apps/kosmos/src/js/main.js
@@ -1,22 +1,26 @@
 $(function() {
 
+	var $posters = $('.poster_item');
+
+	$('.posters_block').append($posters.sort(function() {
+		return 0.5 - Math.random();
+	}));
+
 	var timer = function() {
 		$('.active').trigger('click');
 	};
 
 	var interval = setInterval(timer, 3000);
 
-	var $images = $('.poster_item');
-
-	$images.on('click', function(e) {
+	$posters.on('click', function(e) {
 
 		clearInterval(interval);
 		interval = setInterval(timer, 3000);
 
-		var flag_round = $(this).index() < $images.length - 1;
+		var flag_round = $(this).index() < $posters.length - 1;
 		var $next_load = flag_round
-			? $images.filter(this).nextAll()
-			: $images.first().nextAll().addBack();
+			? $posters.filter(this).nextAll()
+			: $posters.first().nextAll().addBack();
 
 		$next_load.slice(0, 2).not('.load').each(function() {
 			var $this = $(this);
@@ -24,8 +28,8 @@ $(function() {
 		});
 
 		flag_round
-			? $images.removeClass('active').filter(this).next().addClass('active')
-			: $images.removeClass('active').first().addClass('active');
+			? $posters.removeClass('active').filter(this).next().addClass('active')
+			: $posters.removeClass('active').first().addClass('active');
 
 
 	}).last().trigger('click');
