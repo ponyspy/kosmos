2820413bdc390045ab31b1fbd8dfaeebe6ad8226
ponyspy <desade4me@gmail.com>
2017-08-06 01:28:34 +0300

    fix

--- apps/kosmos/src/js/main.js
+++ apps/kosmos/src/js/main.js
@@ -1,38 +1,38 @@
 $(function() {
 
-	var $posters = $('.poster_item');
-
-	$('.posters_block').append($posters.sort(function() {
-		return 0.5 - Math.random();
-	}));
-
 	var timer = function() {
 		$('.active').trigger('click');
 	};
 
 	var interval = setInterval(timer, 3000);
 
-	$posters.on('click', function(e) {
+	var $posters = $('.poster_item').sort(function() {
+		return 0.5 - Math.random();
+	}).appendTo('.posters_block');
 
-		clearInterval(interval);
-		interval = setInterval(timer, 3000);
+	setTimeout(function() {
 
-		var flag_round = $(this).index() < $posters.length - 1;
-		var $next_load = flag_round
-			? $posters.filter(this).nextAll()
-			: $posters.first().nextAll().addBack();
+		$posters.on('click', function(e) {
 
-		$next_load.slice(0, 2).not('.load').each(function() {
-			var $this = $(this);
-			$this.attr('src', $this.attr('data-src')).addClass('load').removeAttr('data-src');
-		});
+			clearInterval(interval);
+			interval = setInterval(timer, 3000);
 
-		flag_round
-			? $posters.removeClass('active').filter(this).next().addClass('active')
-			: $posters.removeClass('active').first().addClass('active');
+			var flag_round = $(this).index() < $posters.length - 1;
+			var $next_load = flag_round
+				? $posters.filter(this).nextAll()
+				: $posters.first().nextAll().addBack();
 
+			$next_load.slice(0, 2).not('.load').each(function() {
+				var $this = $(this);
+				$this.attr('src', $this.attr('data-src')).addClass('load').removeAttr('data-src');
+			});
 
-	}).last().trigger('click');
+			flag_round
+				? $posters.removeClass('active').filter(this).next().addClass('active')
+				: $posters.removeClass('active').first().addClass('active');
 
+		}).last().trigger('click');
+
+	}, 0);
 
 });
