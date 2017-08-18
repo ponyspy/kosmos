ab5e39fa99af36827151481b237d439809a5f491
ponyspy <desade4me@gmail.com>
2017-08-18 09:16:38 +0300

    office

--- apps/kosmos/src/js/office.js
+++ apps/kosmos/src/js/office.js
@@ -1,5 +1,18 @@
 $(function() {
-	$('.block_cut').on('click', function() {
+	$('.block_cut').on('click', function(e) {
 		$(this).parent().find('.hidden').removeClass('hidden').end().end().remove();
 	});
+
+	$('.block_title').on('click', function(e) {
+		e.preventDefault();
+
+		var hash = $(this).attr('href');
+
+		$('html, body').animate({
+			'scrollTop': $(hash).offset().top
+		}, 400, function() {
+			window.location.hash = hash;
+		});
+	});
+
 });
