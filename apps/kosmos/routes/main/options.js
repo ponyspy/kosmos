9d64f06f75076fc11ac4ec44fac6f94d118fe4ad
ponyspy <desade4me@gmail.com>
2021-10-02 20:06:28 +0300

    sitemap

--- apps/kosmos/routes/main/options.js
+++ apps/kosmos/routes/main/options.js
@@ -5,34 +5,28 @@ module.exports = function(Model, Params) {
 
 	var Work = Model.Work;
 
+
 	module.sitemap = function(req, res, next) {
+		var sm_stream = new sitemap.SitemapStream({ hostname: 'https://' + req.hostname });
+		var links = [
+			{ url: '/' },
+			{ url: '/office' },
+		];
+
+		links.forEach(function(link) {
+			sm_stream.write(link);
+		});
+
 		Work.where('status').ne('hidden').exec(function(err, works) {
-			var get_tree = function(base, works) {
-				return works.reduce(function(prev, work) {
-					if (base.replace('projects', 'project') == work.type) {
-						prev.push({ url: '/' + base + '/' + work._short_id });
-					}
-
-					return prev;
-				}, [{ url: '/' + base }]);
-			};
-
-			var site_map = sitemap.createSitemap ({
-				hostname: 'https://' + req.hostname,
-				// cacheTime: 600000,
-				urls: [
-					{ url: '/' },
-					{ url: '/office' },
-				].concat(get_tree('projects', works))
-				 .concat(get_tree('research', works))
+			works.forEach(function(work) {
+				sm_stream.write({ url: '/' + work.type.replace('project', 'projects') + '/' + work._short_id });
 			});
 
-			site_map.toXML(function (err, xml) {
-				if (err) return next(err);
+			sm_stream.end();
+		});
 
-				res.type('xml').send(xml);
-			});
-
+		sm_stream.pipe(res).on('error', function(err) {
+			return next(err);
 		});
 	};
 
